b2d66a6644ae551e3d4af31695597d656d8c02db
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-05-31 12:28:33 +0530

    pagination is added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Pagination from "./Components/Pagination";
 
-
 const App = () => {
   const data = Array.from({ length: 100 }, (_, i) => i + 1);
   console.log(data);
 
-  return <div className="">
-    <Pagination data = {data} />
-  </div>;
+  return (
+    <div className="bg-zinc-900 h-screen text-white p-10">
+      <Pagination
+        data={data}
+        renderRow={function (rowData) {
+          return <div>Hello`{rowData}`</div>;
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Pagination = ({data}) => {
+const PAGE_SIZE = 10;
+const DEFAULT_PAGE = 1;
+
+const Pagination = ({ data, renderRow, pageSize = PAGE_SIZE }) => {
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  const visibleData = data.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(data.length / pageSize);
+
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
   return (
-    <div>{data.map((d)=> (
-        <div key={d} className="pagination-item">
-            {d}
-        </div>
-    ))}</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center">
+      <div className=" ">
+        {visibleData.map((d) => (
+          <div key={d} className="pagination-item">
+            {renderRow(d)}
+          </div>
+        ))}
+      </div>
+      <div className="pagination-buttons flex items-center justify-center gap-4">
+        <button onClick={handlePreviousPage} disabled={currentPage === 1}
+            className={`px-2.5 py-1 border rounded-md border-blue-400 ${currentPage === 1 ? "cursor-no-drop": "cursor-pointer"} hover:bg-blue-500 hover:border-transparent transition-all ease-in-out duration-300`}
+        >
+          Previous
+        </button>
+        {pageNumbers.map((pageNumber) => (
+          <button
+            className={`px-2.5 py-1 border rounded-md border-blue-400 cursor-pointer hover:bg-blue-500 hover:border-transparent transition-all ease-in-out duration-300
+            ${currentPage === pageNumber ? "bg-blue-500 text-white" : "bg-transparent text-blue-400"}}
+            `}
+            key={pageNumber}
+            onClick={()=> setCurrentPage(pageNumber)}
 
-export default Pagination
+          >
+            {pageNumber}
+          </button>
+        ))}
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}
+            className={`px-2.5 py-1 border rounded-md border-blue-400 ${currentPage === totalPages ? "cursor-no-drop": "cursor-pointer"} hover:bg-blue-500 hover:border-transparent transition-all ease-in-out duration-300`}
+            >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
